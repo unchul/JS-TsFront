b4586e179a48ab21ddd7fd1ee45723e65e971312
GoldenDuck <unchul87@gmail.com>
2025-07-30 17:52:26 +0900

    feat:[임운철]account연결중

---
+++ main-container/src/App.tsx
@@ -0,0 +1,33 @@
+import React, { lazy, Suspense, useEffect, useState } from "react";
+import ReactDOM from "react-dom/client";
+
+import { CircularProgress } from "@mui/material";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+
+import VueAccountAppWrapper from "./VueAccountWrapper.tsx";
+
+const App = () => {
+
+return (
+  <BrowserRouter>
+    <Suspense fallback={<CircularProgress />}>
+
+      <Routes>
+
+        <Route path="/vue-account/*" element={<VueAccountAppWrapper />} />
+      </Routes>
+    </Suspense>
+  </BrowserRouter>
+);
+};
+
+export default App;
+
+const container = document.getElementById("app") as HTMLElement;
+if (!container) {
+  throw new Error("Root container #app not found");
+}
+
+const root = ReactDOM.createRoot(container);
+root.render(<App />);
